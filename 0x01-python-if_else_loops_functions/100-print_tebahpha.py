3f34074a451bc7fb0bc2076d2663225f4b3e26d7
mark-saint <markmphillip@gmail.com>
2023-03-08 02:38:56 +0300

    100-print_tebahpha.py

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpha.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+reversed_list = list(reversed(list(range(97, 123))))
+is_lower = True
+for i in reversed_list:
+    if is_lower:
+        i = chr(i)
+        is_lower = False
+    else:
+        i = chr(i - 32)
+        is_lower = True
+    print(i, end='')
