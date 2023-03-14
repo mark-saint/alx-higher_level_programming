b49690aefa523897a3e28ea842c91a10c5c97fa7
mark-saint <markmphillip@gmail.com>
2023-03-15 02:50:14 +0300

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
-for i in hidden_4.__dir__():
-    if i.startswith("__"):
-        continue
-    print("{}".format(i))
+if __name__ == "__main__":
+    for i in hidden_4.__dir__():
+        if i.startswith("__"):
+            continue
+        print("{}".format(i))
