a37503b9a3e530a46c4ebfc0d9823971bb48c67a
mark-saint <markmphillip@gmail.com>
2023-03-15 23:40:55 +0300

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    for i, element in enumerate(my_list):
+        if element == search:
+            my_list[i] = replace
+    return my_list
