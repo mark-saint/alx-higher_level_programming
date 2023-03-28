0378f0bda71019870c14b47f606e61dde5254406
mark-saint <markmphillip@gmail.com>
2023-03-29 01:18:01 +0300

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Cool stuff
+"""
+
+
+class Square:
+    """
+    Square class
+    """
+    def __init__(self, size=0):
+        """
+        Args:
+            size: sth
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Area here we go
+        """
+        return self.__size ** 2
