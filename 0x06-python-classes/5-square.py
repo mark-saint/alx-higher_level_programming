6af235cfa49d5937a8e27c3191ef6d51452c73b7
mark-saint <markmphillip@gmail.com>
2023-03-29 01:51:20 +0300

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+Cool stuff
+"""
+
+
+class Square:
+    """
+    Square class
+    """
+    def __init__(self, size=0):
+        """
+        Args:
+            size: sth
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Area here we go
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        Returns size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        size setter:
+        Args:
+            value: new value
+        Returns:
+            None
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be as integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """
+        Returns:
+            a print
+        """
+        if self.__size == 0:
+            return
+        for _ in range(self.__size):
+            print("#" * self.__size)
