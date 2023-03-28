0216237f62a072ce6419f978b1252239e159be71
mark-saint <markmphillip@gmail.com>
2023-03-29 00:58:10 +0300

    we

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+square
+"""
+
+
+class Square:
+    """
+    square class
+    """
+    def __init__(self, size):
+        """
+        Args:
+            size: number
+        """
+        self.__size = size
