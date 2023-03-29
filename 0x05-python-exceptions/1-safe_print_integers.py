d0750d5c797c9e250abe9d0c77232747657bf05a
mark-saint <markmphillip@gmail.com>
2023-03-29 04:39:53 +0300

    1-safe_print_integers.py

--- 0x05-python-exceptions/1-safe_print_integers.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def safe_print_list_integers(value):
-    try:
-        print("{:d}".format(value))
-    except ValueError:
-        return False
-    else: 
-        return True
