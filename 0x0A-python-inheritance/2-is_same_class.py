87fa2295b8939476945f2e5f8839327350e8bd2b
mark-saint <markmphillip@gmail.com>
2023-04-12 00:21:51 +0300

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This script does
+stuff
+"""
+
+
+def is_same_class(obj, a_class):
+    '''
+    :param obj
+    :param a_class
+    Returns bool
+    '''
+
+    return type(obj) == a_class
