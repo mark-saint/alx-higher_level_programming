4a2f33e7710b85dacbcb47f495d4f7497b36715b
mark-saint <markmphillip@gmail.com>
2023-04-11 23:55:23 +0300

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This script contains a 
+function
+"""
+
+
+def lookup(obj):
+    '''
+    Function to return
+    methods and attributes
+    of an object
+    '''
+    return dir(obj)
