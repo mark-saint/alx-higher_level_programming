3904498a0fdb79d8805bfe43210f5b618aefd3bf
mark-saint <markmphillip@gmail.com>
2023-04-12 00:49:19 +0300

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+script contains a class
+BaseGeometrt
+'''
+
+
+class BaseGeometry(object):
+    '''
+    this is an empty class
+    '''
+    def area(self):
+        '''
+        one has to implement this
+        '''
+
+        raise Exception("area() is not implemented")
