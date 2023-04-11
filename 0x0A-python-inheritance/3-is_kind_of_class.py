7202db73cdfe74e9dd4220ecc22d033d7a7b1800
mark-saint <markmphillip@gmail.com>
2023-04-12 00:26:07 +0300

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+script implements
+is kind of class
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''
+    returns true if
+    obj isinstance of
+    a class
+    '''
+
+    return isinstance(obj, a_class)
