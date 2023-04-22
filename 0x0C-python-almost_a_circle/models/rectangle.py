01ef6db390ea1c7a4dbf4bea4f1f0da1b47943b6
mark-saint <markmphillip@gmail.com>
2023-04-22 04:12:44 +0300

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,32 +25,40 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """This is a setter for x"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """This is a getter for x"""
         self.__x = value
 
     @property
     def y(self):
+        """This is a setter for y"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """This is a getter for y"""
         self.__y = value
 
     @property
     def width(self):
+        """This is a getter for width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """This is a setter for width"""
         self.__width = value
 
     @property
     def height(self):
+        """This is a getter for height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """This is a setter for height"""
         self.__height = height
