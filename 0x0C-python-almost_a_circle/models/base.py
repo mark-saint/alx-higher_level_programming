22288d14076e0ab41d9f5537d9655b73ae6899f9
mark-saint <markmphillip@gmail.com>
2023-05-18 15:57:51 +0300

    models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,9 @@ This is a base script
 """
 
 
+import json
+
+
 class Base:
     """
     This is a base script
@@ -20,3 +23,16 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        does stuff
+        """
+        json_dict = []
+        if len(list_dictionaries) != 0:
+            for dict_ in list_dictionaries:
+                json_dict.append(json.dumps(dict_))
+            return json_dict
+        else:
+            return list_dictionaries
