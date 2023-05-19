7b46adb56fdf7593f63ea36178b480fe4f70100f
mark-saint <markmphillip@gmail.com>
2023-05-19 15:25:15 +0300

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,6 +3,6 @@
 
 CREATE TABLE IF NOT EXISTS second_table (id int, name varchar(256), score int);
 INSERT INTO second_table (id, name, score) VALUES (1, "john", 10);
-INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 10);
-INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 10);
-INSERT INTO second_table (id, name, score) VALUES (4, "George", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
