9e3aa110a3a5fae33a30d50dc8f0a508f918840c
mark-saint <markmphillip@gmail.com>
2023-05-20 12:19:17 +0300

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- dfasdfdsafd
 
 
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
-GRANT ALL PRIVILEGES ON "." TO user_0d_1@localhost WITH GRANT OPTION;
+CREATE DATABASE IF NOT EXISTS user_0d_1;
+GRANT ALL PRIVILEGES ON user_0d_1 TO user_0d_1@localhost identified BY user_0d_1_pwd;
