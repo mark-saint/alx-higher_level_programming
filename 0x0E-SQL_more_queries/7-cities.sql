4508c2291c4150d0ece06ec225ff4e0c94d6afa7
mark-saint <markmphillip@gmail.com>
2023-05-20 13:57:54 +0300

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,6 +7,6 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	state_id int NOT NULL,
 	name varchar(256) NOT NULL,
 	PRIMARY KEY(id),
-	FOREIGN KEY(state_id) REFERENCES states(id)
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
 	)
 
