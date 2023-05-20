45c4fd18cdd7bd5cfdc162c2e43684fd1a6025a8
mark-saint <markmphillip@gmail.com>
2023-05-20 13:10:54 +0300

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,5 +2,5 @@
 
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
 (id int UNIQUE NOT NULL, nmae varchar(256) NOT NULL);
