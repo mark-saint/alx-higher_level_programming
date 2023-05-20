820179316f1652b49f01b8d66400c1f5bf8c15dc
mark-saint <markmphillip@gmail.com>
2023-05-20 13:19:50 +0300

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -4,5 +4,5 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
 (id int AUTO_INCREMENT UNIQUE NOT NULL,
-name varchar(256) NOT NULL
+name varchar(256) NOT NULL,
 PRIMARY KEY (id));
