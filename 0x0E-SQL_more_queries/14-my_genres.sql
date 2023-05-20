0a785df6738f6e4a5d317bbc8ce6e471b3bc244f
mark-saint <markmphillip@gmail.com>
2023-05-20 16:32:49 +0300

    14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,7 +4,7 @@
 SELECT tv_genres.name 
 FROM tv_genres
 INNER JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genres_id
+ON tv_genres.id = tv_show_genres.genre_id
 
 INNER JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
