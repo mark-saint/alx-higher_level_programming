7e8d58642e5c54d5a26137a0cfd052c5befd41b9
mark-saint <markmphillip@gmail.com>
2023-05-20 15:38:31 +0300

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,5 +5,5 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-COUNT(tv_show_genres.genre_id) => 1
+-- COUNT(tv_show_genres.genre_id) => 1
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
