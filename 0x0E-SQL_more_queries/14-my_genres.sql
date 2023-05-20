cdeeb54fb2598e42da3eb1f5586b9509d4904989
mark-saint <markmphillip@gmail.com>
2023-05-20 16:31:36 +0300

    14-my_genres.sql

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,11 @@
+-- dsfasdf
+
+
+SELECT tv_genres.name 
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genres_id
+
+INNER JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show.title = "Dexter";
