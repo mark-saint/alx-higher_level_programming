2cca634eb46e717707feb9b6dba125a1c1aaa8b1
mark-saint <markmphillip@gmail.com>
2023-05-25 23:49:32 +0300

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+kfadslkjdsfjkdfs
+"""
+
+
+def read_file(file_name=""):
+    """
+    reads a file
+    """
+    with open(file_name, 'r', encoding='utf-8') as f:
+        print(f.read())
