74f230384ec113e9088bebf606596de50c1b4068
mark-saint <markmphillip@gmail.com>
2023-05-26 01:07:42 +0300

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+asdfsdf
+"""
+
+
+class Student:
+    """
+    dsfs
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        sadf
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        asfasdf
+        """
+        return self.__dict__
