2e2a53e7d6f7bdb52b652967e70f441c9dcef5da
mark-saint <markmphillip@gmail.com>
2023-05-25 23:57:19 +0300

    dsf

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+sdfjndflk
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    writes a file
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(text)
+
+    return len(text)
+        
