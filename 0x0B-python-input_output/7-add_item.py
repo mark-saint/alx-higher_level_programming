0f2881269cca8d9f171341b4dfa8947b1604b60c
mark-saint <markmphillip@gmail.com>
2023-05-26 00:59:02 +0300

    7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+asfsd
+"""
+
+
+import json
+import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+filename = "add_item.json"
+try:
+    list_ = load_from_json_file(filename)
+except FileNotFoundError:
+    list_ = []
+
+n =  len(sys.argv)
+
+for i in range(1, n):
+    list_.append(sys.argv[i])
+
+save_to_json_file(list_, "add_item.json")
