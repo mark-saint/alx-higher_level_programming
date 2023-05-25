b2fc1cc3b55d110812eb1165c63fb9f70c821c5d
mark-saint <markmphillip@gmail.com>
2023-05-26 00:03:11 +0300

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+sadfsdafds
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    sdafads
+    """
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
